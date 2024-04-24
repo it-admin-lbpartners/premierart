16454b2d9ac1bb597da3a6d914be3a9431768f92
Arno <it-admin@lbpartners.be>
2024-04-24 11:54:01 +0200

    changements

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-[calc(100vh-var(--header-height))]">
     <img src="images/premier-art-logo--rgb--horizontal-white.png" alt="" srcset="">
-    <h1>Site en construction</h1>
+    <h1 class="text-4xl lg:text-8xl">Site en maintenance</h1>
   </div>
 </template>
 
